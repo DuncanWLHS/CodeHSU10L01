3c70c0aae27438bf11e8d52c84a39eeae69cdd12
Duncan Duffield <duffied1@wlhs.wlwv.k12.or.us>
2023-04-19 09:48:20 -0700

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        RecursiveCountdown(10);
+        System.out.println(SumOfNumbers(4));
+        System.out.println(factorial(10));
+        int arr[] = {1,2,3};
+        System.out.println(sumArray(arr, arr.length));
+    }
+
+    public static int SumOfNumbers(int n) {
+        //base case
+        if (n == 1) {
+            return 1;
+        }
+        return n + SumOfNumbers(n - 1);
+    }
+
+    public static int factorial(int n) {
+        //base case
+        if (n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void RecursiveCountdown(int n) {
+        //base case
+        if (n == 1) {
+            System.out.println(1);
+        } else {
+            System.out.print(n + " ");
+            RecursiveCountdown(n - 1);
+        }
+    }
+
+    public  static int sumArray(int[] arrayIn, int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return arrayIn[n-1] + sumArray(arrayIn, n-1);
     }
 }
